0a0255837e8870f702ddae3cf33713328582a816
saftpampe <mateo.hofmeier@bluewin.ch>
2025-03-20 15:05:22 +0100

    initial commit

---
+++ HockeyApp_BE/HockeyApp/src/main/java/ch/iet_gibb/hockeyapp/HockeyAppApplication.java
@@ -0,0 +1,13 @@
+package ch.iet_gibb.hockeyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HockeyAppApplication.class, args);
+    }
+
+}
---
+++ HockeyApp_BE/HockeyApp/src/test/java/ch/iet_gibb/hockeyapp/HockeyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.iet_gibb.hockeyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
